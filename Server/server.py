22a1931f6434e2debdad296b838275ae99884f1e
mustafanoman128 <mustafanoman128@gmail.com>
2026-05-10 12:49:06 +0500

    prepare for Render deployment

--- Server/server.py
+++ Server/server.py
@@ -1,7 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import util
+import os
 
-app = Flask(__name__)
+# Load ML model and location stats at import time so Gunicorn picks it up
+util.load_saved_artifacts()
+
+CLIENT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'Client')
+app = Flask(__name__, static_folder=CLIENT_DIR, static_url_path='')
+
+
+@app.route('/')
+def serve_frontend():
+    return send_from_directory(app.static_folder, 'app.html')
+
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
@@ -11,6 +22,7 @@ def get_location_names():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+
 @app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
@@ -18,9 +30,9 @@ def predict_home_price():
     bhk        = int(request.form['bhk'])
     bath       = int(request.form['bath'])
 
-    price         = util.get_estimated_price(location, total_sqft, bhk, bath)
+    price          = util.get_estimated_price(location, total_sqft, bhk, bath)
     price_per_sqft = round(price * 100000 / total_sqft) if total_sqft > 0 else 0
-    context       = util.get_location_context(location, price_per_sqft)
+    context        = util.get_location_context(location, price_per_sqft)
 
     response = jsonify({
         'estimated_price': price,
@@ -29,7 +41,7 @@ def predict_home_price():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
     app.run()
